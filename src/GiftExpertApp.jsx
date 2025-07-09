0318c79e8aefe81e89904e90db29d772985c6ec6
Bastian Rodríguez <basrodriguezr@gmail.com>
2025-07-09 15:53:57 -0400

    se usa desestructuracion para conservar las categorias anteriores en funcion onAddCategory, se agrega un input para tipear el nombre de la categoria y un boton para agregarla, ademas se agrega que si se apreta la tecla Enter esta debe hacer la accion de agregar categoria

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -3,14 +3,41 @@ import { useState } from 'react';
 export const GiftExpertApp = () => {
     const [categories, setCategories] = useState(['One Piece', 'Naruto', 'Dragon Ball']);
 
+    const onAddCategory = (newCategory) => {
+        if (categories.includes(newCategory)) {
+            return;
+        }
+        setCategories([newCategory, ...categories]);        
+    };
+
     return (
         <>
         {/*Titulo*/}
         <h1>Gift Expert App</h1>
         <hr />
         {/*input*/}
-
+       <input type="text" placeholder="Agregar Categoria"
+            onKeyDown={(event) => { 
+                // Verificar si la tecla presionada es Enter y el input no está vacío
+                // Si es así, agregar la categoría vía onkeydown
+                if (event.key === 'Enter' && event.target.value.trim().length > 0) {
+                    onAddCategory(event.target.value.trim());
+                    event.target.value = ''; // Limpiar el input
+                }
+            }} 
+        />
+        
+        <button onClick={() => {
+            const input = document.querySelector('input');  
+            if (input.value.trim().length > 0) {
+                onAddCategory(input.value.trim());
+                input.value = ''; // Limpiar el input
+            }
+        }
+        }>Agregar</button>
+        <hr />
         {/*Listado de gifts*/}
+        <h2>Categorias</h2>
         <ol>
             {
                 categories.map(category => (
