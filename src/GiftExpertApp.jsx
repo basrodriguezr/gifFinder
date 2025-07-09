195b36021ecfe16ba22bffb7cce84e3737ff5578
Bastian Rodríguez <basrodriguezr@gmail.com>
2025-07-09 14:53:55 -0400

    primer commit, proyecto inicial Gift Expert APP

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,11 @@
+
+export const GiftExpertApp = () => {
+  return (
+    <div>
+      <h1>Gift Expert App</h1>
+      <hr />
+      <p>Search for your favorite gifts!</p>
+      {/* Add your components here */}
+    </div>
+  );
+}
