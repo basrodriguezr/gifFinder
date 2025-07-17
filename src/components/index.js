c2d032829e3e8cfe8d755748263fb335dadf6fdb
Bastian Rodríguez <basrodriguezr@gmail.com>
2025-07-17 14:45:29 -0400

    se agrega archivo de barril que contiene la carga de todos los componentes

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+//si uno se refiere solo a la carpeta, por ejemplo, src/components, 
+// sin el componente que se quiere cargar, por default se irá a buscar un archivo "index", por lo que creamos este archivo
+
+export * from './AddCategory';
+export * from './GifGrid';
+export * from './GifItem';
